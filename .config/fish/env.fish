269f595f38f0cf2ff40334f677b4e270488ac120
Shichisan <taiga.shichisan@gmail.com>
2020-08-10 01:10:09 +0900

    :recycle: update document

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,15 +1,21 @@
 # default editor
 set -U EDITOR nvim
+
 # tmux
 set -x TERM xterm-256color
-# neovim python
+
+# neovim python path
 set -x NVIM_PYTHON_LOG_FILE /tmp/log
 set -x NVIM_PYTHON_LOG_LEVEL DEBUG
+
+# neovim ruby path
 set -x NVIM_RUBY_LOG_FILE /tmp/log
 set -x NVIM_RUBY_LOG_LEVEL DEBUG
+
 # anyenv paths
-set PATH $HOME/.anyenv/bin $PATH
+set -x PATH $HOME/.anyenv/bin $PATH
 source (anyenv init - | psub)
+
 for D in rbenv pyenv nodenv goenv
   set -x PATH $HOME/.anyenv/envs/$D/shims $PATH
 end
@@ -17,8 +23,12 @@ set -x GOPATH $HOME/dev
 set -x PATH $GOPATH/bin $PATH
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# imagemagick
 set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
 # set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
+
+# mysql
 
 # flutter
 set -x PATH $HOME/dev/src/flutter/bin $PATH
