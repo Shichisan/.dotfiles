755083b9486d61e7136f94bb7bce5692227cc78b
Shichisan <taiga.shichisan@gmail.com>
2020-11-16 13:35:35 +0900

    :+1: use fzf instead of peco

--- .config/fish/conf.d/ghq.fish
+++ .config/fish/conf.d/ghq.fish
@@ -2,9 +2,4 @@ function __ghq_cd_repository -d "Change local repository directory"
   ghq list --full-path | fzf --reverse | read -l repo_path
   cd $repo_path
 end
-alias ghf __ghq_cd_repository
-
-function __ghq_peco_src -d "Change local repository by peco"
-  ghq list --full-path | peco --query "$LBUFFER"
-end
-bind \ck __ghq_peco_src
+bind \ck __ghq_cd_repository
