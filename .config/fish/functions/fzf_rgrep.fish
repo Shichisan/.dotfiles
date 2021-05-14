d0fc55c362cf57bfb2c76dbc158aa9d561d1602d
Shichisan <taiga.shichisan@gmail.com>
2021-05-15 02:04:29 +0900

    :shower: remove bugged preview

--- .config/fish/functions/fzf_rgrep.fish
+++ .config/fish/functions/fzf_rgrep.fish
@@ -3,6 +3,5 @@ function fzf_rgrep -d "fuzzy search by ripgrep"
   set RG_PREFIX "rg --column --line-number --no-heading --color=always --smart-case "
   set FZF_DEFAULT_COMMAND "$RG_PREFIX '$INITIAL_QUERY'"
   fzf --bind "change:reload:$RG_PREFIX {q} || true" \
-      --ansi --disabled --query "$INITIAL_QUERY" \
-      --preview "cat `echo {} | cut -f 1 -d ':'`"
+      --ansi --disabled --query "$INITIAL_QUERY"
 end
