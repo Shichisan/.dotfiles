75af55d0f711d5a3554a682eaed2f5872446a01d
Shichisan <taiga.shichisan@gmail.com>
2021-05-15 00:28:19 +0900

    :+1: add preview add file matcher

--- .config/fish/functions/repository_fuzzy_matcher.fish
+++ .config/fish/functions/repository_fuzzy_matcher.fish
@@ -1,5 +1,5 @@
 function repository_fuzzy_matcher -d "Fuzzy matcher for moving directory"
-  ghq list | fzf --reverse --height=100% | read -l selected
+  ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :500 (ghq root)/{}/README.*" | read -l selected
   ghq root | read -l ghq_root
   [ -n "$selected" ]; and cd "$ghq_root/$selected"
   echo "$selected "
