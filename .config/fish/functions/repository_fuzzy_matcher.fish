d3573e1dd7dc64475932385dbb1f982c7d5c13a7
Shichisan <taiga.shichisan@gmail.com>
2021-05-15 00:36:55 +0900

    :+1: change line-range of preview by fzf

--- .config/fish/functions/repository_fuzzy_matcher.fish
+++ .config/fish/functions/repository_fuzzy_matcher.fish
@@ -1,5 +1,5 @@
 function repository_fuzzy_matcher -d "Fuzzy matcher for moving directory"
-  ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :500 (ghq root)/{}/README.*" | read -l selected
+  ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :100 (ghq root)/{}/README.*" | read -l selected
   ghq root | read -l ghq_root
   [ -n "$selected" ]; and cd "$ghq_root/$selected"
   echo "$selected "
